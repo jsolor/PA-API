75803f4b1e9dab17995c757b5438b2cb066410c3
Jesus Solorzano <jesus.solorz@gmail.com>
2023-02-22 16:50:10 -0500

    merge conflicts

--- server/database.js
+++ server/database.js
@@ -1,12 +1,15 @@
+/* eslint-disable max-len */
+/* eslint-disable no-unused-vars */
 const { Client } = require('pg');
 const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '../.env') });
 
-const client = new Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'sdc',
-  password: 'pg',
-  port: '5432',
+const db = new Client({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
 });
 
-module.exports = client;
+module.exports = db;
